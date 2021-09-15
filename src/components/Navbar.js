9758332342bb90b3fb6964a284d1496e8b7c62e3
MuratDikmen <mrtdkmn@gmail.com>
2021-09-15 10:59:48 -0400

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <div className="text-red-300">Spacestagram</div>;
+}
+
+export default Navbar;
---
+++ src/views/Feed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Feed() {
+  return <div>Image Feed</div>;
+}
+
+export default Feed;
